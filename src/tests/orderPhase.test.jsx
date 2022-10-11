a7083ba85d1214dc7b49f844cd7c8e9ff04d811d
Michael Drummond <michael.drummond328@gmail.com>
2022-10-10 18:20:04 -0600

    fixed orderPhase tests for mockup changes and async await errors

--- src/tests/orderPhase.test.jsx
+++ src/tests/orderPhase.test.jsx
@@ -3,7 +3,6 @@ import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 test("order phases for happy path", async () => {
-  const user = userEvent.setup();
   // render app
   render(<App />);
 
@@ -15,28 +14,30 @@ test("order phases for happy path", async () => {
     name: "Hot fudge",
   });
 
-  await user.clear(chocolateScoop);
-  await user.type(chocolateScoop, "2");
-  await user.click(hotFudgeTopping);
+  userEvent.clear(chocolateScoop);
+  userEvent.type(chocolateScoop, "2");
+  userEvent.click(hotFudgeTopping);
 
   // find and click order button
   const orderBtn = await screen.findByRole("button", { name: "Order Sundae!" });
-  await user.click(orderBtn);
+  userEvent.click(orderBtn);
 
   // check summary information based on order
   const scoopsSubtotal = screen.getByRole("heading", { name: /scoops: \$/i });
   const toppingsSubtotal = screen.getByRole("heading", {
     name: /toppings: \$/i,
   });
-  const grandTotal = screen.getByRole("heading", { name: /total \$/i });
 
   expect(scoopsSubtotal).toHaveTextContent("4.00");
   expect(toppingsSubtotal).toHaveTextContent("1.50");
-  expect(grandTotal).toHaveTextContent("5.50");
+
+  // removed due to mockup change, can be uncommented if reverted back
+  // const grandTotal = screen.getByRole("heading", { name: /total \$/i });
+  // expect(grandTotal).toHaveTextContent("5.50");
 
   const optionItems = screen.getAllByRole("listitem");
   const optionItemsText = optionItems.map((item) => item.textContent);
-  expect(optionItemsText).toEqual(["2 Chocolate", "1 Hot fudge"]);
+  expect(optionItemsText).toEqual(["2 Chocolate", "Hot fudge"]);
   // OR
   // expect(screen.getByText("2 Chocolate")).toBeInTheDocument();
   // expect(screen.getByText("1 Hot fudge")).toBeInTheDocument();
@@ -47,8 +48,8 @@ test("order phases for happy path", async () => {
   });
   const confirmOrderBtn = screen.getByRole("button", { name: "Confirm order" });
 
-  await user.click(termsConditionsCheckbox);
-  await user.click(confirmOrderBtn);
+  userEvent.click(termsConditionsCheckbox);
+  userEvent.click(confirmOrderBtn);
 
   const thankYouHeader = await screen.findByRole("heading", {
     name: /thank you/i,
@@ -56,25 +57,21 @@ test("order phases for happy path", async () => {
   expect(thankYouHeader).toBeInTheDocument();
 
   // confirm order number on confirmation page
-  const orderNumber = await screen.findByRole("heading", {
-    name: "Your order number is 2134823570",
-  });
+  const orderNumber = await screen.findByText(/order number/i);
   expect(orderNumber).toBeInTheDocument();
 
   // click "new order" button on confirmation page
   const newOrderBtn = screen.getByRole("button", { name: "Create new order" });
-  await user.click(newOrderBtn);
+  userEvent.click(newOrderBtn);
 
   // check that the scoops and toppings subtotals have been reset
-  const scoopsSubtotalReset = screen.getByRole("heading", {
-    name: /scoops: \$/i,
-  });
-  const toppingsSubtotalReset = screen.getByRole("heading", {
-    name: /toppings: \$/i,
-  });
+  const scoopsSubtotalReset = await screen.findByText("Scoops total: $0.00");
+  const toppingsSubtotalReset = await screen.findByText(
+    "Toppings total: $0.00"
+  );
 
-  expect(scoopsSubtotalReset).toHaveTextContent("0.00");
-  expect(toppingsSubtotalReset).toHaveTextContent("0.00");
+  expect(scoopsSubtotalReset).toBeInTheDocument();
+  expect(toppingsSubtotalReset).toBeInTheDocument();
 
   // do we need to await anything to avoid test errors?
   // these are awaited at end of test to ensure no test errors from component updates after test is complete
